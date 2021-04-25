98e4471133693ddae7067b03435d14af61922278
Chae Rim Kim <kcl0720@gmail.com>
2021-04-26 00:53:39 +0900

    Restructure the project folder

---
+++ interface/src/layout/User.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <navbar />
+    <main>
+      <section class="relative w-full h-full py-40 min-h-screen">
+        <router-view />
+        <!-- <footer-small absolute /> -->
+      </section>
+    </main>
+  </div>
+</template>
+
+<script>
+import Navbar from "../components/Navbar.vue";
+
+export default {
+  data() {
+    return {
+    //   registerBg2,
+    };
+  },
+  components: {
+    Navbar,
+    // FooterSmall,
+  },
+};
+</script>
